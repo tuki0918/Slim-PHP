5501791c3d5d3e71172c914ffcc731b70c0c042a
yuta yamamoto <tuki0918@gmail.com>
2015-06-24 20:32:13 +0900

    tmp

--- app/Config/eloquent.php
+++ app/Config/eloquent.php
@@ -8,3 +8,39 @@ $db = new DB;
 $db->addConnection($config);
 $db->setAsGlobal();
 $db->bootEloquent();
+
+class_alias('Cartalyst\Sentry\Facades\Native\Sentry', 'Sentry');
+
+// Sentry::register(array(
+//     'email'    => 'john.doe@example.com',
+//     'password' => 'foobar',
+// ));
+
+// $user = Sentry::findUserByLogin('john.doe@example.com');
+
+// var_dump($user->email);
+
+// Sentry::authenticate(array(
+//     'email'    => 'john.doe@example.com',
+//     'password' => 'foobar',
+// ));
+
+// Sentry::createGroup(array(
+//     'name'        => 'Subscribers',
+//     'permissions' => array(
+//         'admin' => 1,
+//         'users' => 1,
+//     ),
+// ));
+
+
+$credentials = array(
+    'email'    => 'john.doe@example.com',
+    'password' => 'foobar',
+);
+
+// Authenticate the user
+$user = Sentry::authenticate($credentials, false);
+
+var_dump($user);
+
